55c46af062d29d838295b4762dd8b6be377b4555
Jhonathan Segura <jhonathanssegura@gmail.com>
2024-07-26 21:11:32 -0500

    Adding base project

---
+++ src/main/java/com/real_time_notification/RealTimeNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.real_time_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/real_time_notification/RealTimeNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.real_time_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
